0a92d8ce3d69e3ed376b559dd39bd6566e7f079b
SQA-1350084 <55232726+SQA-1350084@users.noreply.github.com>
2020-01-23 12:11:52 +0000

    Add files via upload

---
+++ Game 1/Game 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the game");
+            Console.Write("You have entered the Game. would you like to conteniue Y/N");
+            String inputOne = Console.ReadLine();
+            String inputTwo = Console.ReadLine();
+
+            if (inputOne == "Y")
+            {
+                Console.WriteLine("it is your Choise");
+                Console.ReadLine();
+
+
+            }
+            else 
+            {
+                Console.WriteLine("");
+ 
+
+
+            }
+
+            Console.Write("Are you Ok? Y/N");
+            String inputThree = Console.ReadLine();
+            String inputFour = Console.ReadLine();
+
+            if (inputThree == "Y")
+            {
+                Console.WriteLine("Nice");
+                Console.ReadLine();
+
+
+            }
+            else 
+            {
+                Console.WriteLine("ooh, sorry. no i im not ");
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
